120bfed65ea3d76d5cf233e56e59ad811879ab62
jsvictorias <juliasantos7652@gmail.com>
2024-09-16 19:06:06 -0300

    função de iniciar o timer

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,37 @@ const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
+let segundos = 0;
+let data;
+
+
+function showTime(segundos){
+    data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12:false,
+        timeZone: 'UTC'
+    });
+}
+
+function iniciaRelogio(){
+    const seg = setInterval(function(){
+        segundos++;
+        relogio.innerHTML = showTime(segundos);
+    }, 1000)
+    return seg
+}
 
 iniciar.addEventListener('click', function(event) {
-    alert('cliquei no iniciar');
+    // alert('cliquei no iniciar');
+    iniciaRelogio()
 });
 
+
+
 pausar.addEventListener('click', function(event){
-    alert('cliquei no pausar');
+    // alert('cliquei no pausar');
+    
+
 });
 
 zerar.addEventListener('click', function(){
